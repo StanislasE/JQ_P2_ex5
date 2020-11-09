bef47f52f738a0cd8c1fd02e013afaf47c4ac2ee
StanislasE <stanislas.etienne@gmail.com>
2020-11-09 12:03:28 +0100

    fait

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$('#green')
+    .mouseenter(function() {
+        $('#text').css('color', '#008000');
+    })
+    .mouseout(function(){
+        $('#text').css('color', '#000000')
+    });
+$('#red')
+    .mouseenter(function() {
+        $('#text').css('color', '#FF0000');
+    })
+    .mouseout(function(){
+        $('#text').css('color', '#000000')
+    }); 
+$('#blue')
+    .mouseenter(function() {
+        $('#text').css('color', '#0000FF');
+    })
+    .mouseout(function(){
+        $('#text').css('color', '#000000')
+    });     
